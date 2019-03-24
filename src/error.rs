6a913b0b9d0a498d8b150d3a6b6b289ddcd8772b
Florian Merz <flomerz@gmail.com>
2019-03-24 18:19:09 +0100

    move to failure

---
+++ src/error.rs
@@ -0,0 +1,52 @@
+#[cfg(feature = "aws")]
+use rusoto_ssm::GetParameterError;
+
+#[derive(Debug, Fail)]
+pub enum RsaKeyError {
+    #[fail(display = "no d")]
+    NoD,
+    #[fail(display = "no p")]
+    NoP,
+    #[fail(display = "no q")]
+    NoQ,
+    #[fail(display = "no dp")]
+    NoDP,
+    #[fail(display = "no dq")]
+    NoDQ,
+    #[fail(display = "no qi")]
+    NoQI,
+}
+
+#[derive(Debug, Fail)]
+pub enum KeyError {
+    #[fail(display = "no rsa jwk")]
+    NoRsaJwk,
+}
+
+#[derive(Debug, Fail)]
+pub enum SignerVerifierError {
+    #[fail(display = "no publisher")]
+    NoPublisher,
+    #[fail(display = "attribute must be an object")]
+    NonObjectAttribute,
+    #[fail(display = "keys missing for publihser {}", _0)]
+    NoPublisherKeys(String),
+}
+
+#[cfg(feature = "aws")]
+#[derive(Debug, Fail)]
+pub enum SsmError {
+    #[fail(display = "{}", _0)]
+    GetParamterFailed(GetParameterError),
+    #[fail(display = "no parameter")]
+    NoParameter,
+    #[fail(display = "no value")]
+    NoValue,
+}
+
+#[cfg(feature = "well_known")]
+#[derive(Debug, Fail)]
+pub enum WellKnownError {
+    #[fail(display = "unable to retrieve verify keys")]
+    RetrieveVerifyKeysFailed,
+}
--- src/keys.rs
+++ src/keys.rs
@@ -1,9 +1,12 @@
+use crate::error::KeyError;
+use crate::error::RsaKeyError;
 use biscuit::jwk::AlgorithmParameters;
 use biscuit::jwk::RSAKeyParameters;
 use biscuit::jwk::JWK;
 use biscuit::jws;
 use biscuit::jws::Secret;
 use biscuit::Empty;
+use failure::Error;
 use num_bigint::BigUint;
 use openssl::bn::BigNum;
 use openssl::rsa::Rsa;
@@ -13,7 +16,7 @@ use serde::Deserialize;
 use std::sync::Arc;
 use untrusted;
 
-pub fn verify_key_from_str(s: &str) -> Result<Secret, String> {
+pub fn verify_key_from_str(s: &str) -> Result<Secret, Error> {
     if s.starts_with("-----BEGIN RSA PRIVATE KEY-----") {
         verify_key_from_private_pem(s)
     } else if s.starts_with("-----BEGIN PUBLIC KEY-----") {
@@ -23,7 +26,7 @@ pub fn verify_key_from_str(s: &str) -> Result<Secret, String> {
     }
 }
 
-pub fn sign_key_from_str(s: &str) -> Result<Secret, String> {
+pub fn sign_key_from_str(s: &str) -> Result<Secret, Error> {
     if s.starts_with("-----BEGIN RSA PRIVATE KEY-----") {
         sign_key_from_pem(s)
     } else {
@@ -31,22 +34,16 @@ pub fn sign_key_from_str(s: &str) -> Result<Secret, String> {
     }
 }
 
-fn sign_key_from_pem(key: &str) -> Result<Secret, String> {
-    let rsa = Rsa::private_key_from_pem(key.as_bytes())
-        .map_err(|e| format!("error reading key: {}", e))?;
-    let sign = jws::Secret::RSAKeyPair(Arc::new(
-        RSAKeyPair::from_der(untrusted::Input::from(
-            &rsa.private_key_to_der()
-                .map_err(|e| format!("error converting key: {}", e))?,
-        ))
-        .map_err(|e| format!("error converting to der: {}", e))?,
-    ));
+fn sign_key_from_pem(key: &str) -> Result<Secret, Error> {
+    let rsa = Rsa::private_key_from_pem(key.as_bytes())?;
+    let sign = jws::Secret::RSAKeyPair(Arc::new(RSAKeyPair::from_der(untrusted::Input::from(
+        &rsa.private_key_to_der()?,
+    ))?));
     Ok(sign)
 }
 
-fn verify_key_from_public_pem(key: &str) -> Result<Secret, String> {
-    let rsa = Rsa::public_key_from_pem(key.as_bytes())
-        .map_err(|e| format!("error reading key: {}", e))?;
+fn verify_key_from_public_pem(key: &str) -> Result<Secret, Error> {
+    let rsa = Rsa::public_key_from_pem(key.as_bytes())?;
     let verify = jws::Secret::RSAModulusExponent {
         n: BigUint::from_bytes_be(&rsa.n().to_vec()),
         e: BigUint::from_bytes_be(&rsa.e().to_vec()),
@@ -54,9 +51,8 @@ fn verify_key_from_public_pem(key: &str) -> Result<Secret, String> {
     Ok(verify)
 }
 
-fn verify_key_from_private_pem(key: &str) -> Result<Secret, String> {
-    let rsa = Rsa::private_key_from_pem(key.as_bytes())
-        .map_err(|e| format!("error reading key: {}", e))?;
+fn verify_key_from_private_pem(key: &str) -> Result<Secret, Error> {
+    let rsa = Rsa::private_key_from_pem(key.as_bytes())?;
     let verify = jws::Secret::RSAModulusExponent {
         n: BigUint::from_bytes_be(&rsa.n().to_vec()),
         e: BigUint::from_bytes_be(&rsa.e().to_vec()),
@@ -64,31 +60,30 @@ fn verify_key_from_private_pem(key: &str) -> Result<Secret, String> {
     Ok(verify)
 }
 
-pub fn sign_key_from_jwk(key: &str) -> Result<Secret, String> {
+pub fn sign_key_from_jwk(key: &str) -> Result<Secret, Error> {
     let rsa = jwk_str_to_rsa_key_params(key)?;
-    let sign = jws::Secret::RSAKeyPair(Arc::new(
-        RSAKeyPair::from_der(untrusted::Input::from(&to_der(rsa)?))
-            .map_err(|e| format!("error converting to der: {}", e))?,
-    ));
+    let sign = jws::Secret::RSAKeyPair(Arc::new(RSAKeyPair::from_der(untrusted::Input::from(
+        &to_der(rsa)?,
+    ))?));
     Ok(sign)
 }
 
-pub fn verify_key_from_jwk(key: &str) -> Result<Secret, String> {
+pub fn verify_key_from_jwk(key: &str) -> Result<Secret, Error> {
     let rsa = jwk_str_to_rsa_key_params(key)?;
     let verify = rsa.jws_public_key_secret();
     Ok(verify)
 }
 
 #[allow(clippy::many_single_char_names)]
-pub fn to_der(jwk_rsa: RSAKeyParameters) -> Result<Vec<u8>, String> {
+pub fn to_der(jwk_rsa: RSAKeyParameters) -> Result<Vec<u8>, Error> {
     let n = jwk_rsa.n;
     let e = jwk_rsa.e;
-    let d = jwk_rsa.d.ok_or_else(|| String::from("no d"))?;
-    let p = jwk_rsa.p.ok_or_else(|| String::from("no p"))?;
-    let q = jwk_rsa.q.ok_or_else(|| String::from("no q"))?;
-    let dp = jwk_rsa.dp.ok_or_else(|| String::from("no dp"))?;
-    let dq = jwk_rsa.dq.ok_or_else(|| String::from("no dq"))?;
-    let qinv = jwk_rsa.qi.ok_or_else(|| String::from("no q"))?;
+    let d = jwk_rsa.d.ok_or_else(|| RsaKeyError::NoD)?;
+    let p = jwk_rsa.p.ok_or_else(|| RsaKeyError::NoP)?;
+    let q = jwk_rsa.q.ok_or_else(|| RsaKeyError::NoQ)?;
+    let dp = jwk_rsa.dp.ok_or_else(|| RsaKeyError::NoDP)?;
+    let dq = jwk_rsa.dq.ok_or_else(|| RsaKeyError::NoDQ)?;
+    let qinv = jwk_rsa.qi.ok_or_else(|| RsaKeyError::NoQI)?;
 
     let builder = RsaPrivateKeyBuilder::new(
         BigNum::from_slice(&n.to_bytes_be()).unwrap(),
@@ -113,17 +108,16 @@ pub fn to_der(jwk_rsa: RSAKeyParameters) -> Result<Vec<u8>, String> {
     Ok(der)
 }
 
-pub fn jwk_str_to_rsa_key_params(key: &str) -> Result<RSAKeyParameters, String> {
+pub fn jwk_str_to_rsa_key_params(key: &str) -> Result<RSAKeyParameters, Error> {
     let mut j = serde_json::Deserializer::from_str(key);
-    let jwk: JWK<Empty> =
-        JWK::deserialize(&mut j).map_err(|e| format!("error reading jwk: {}", e))?;
+    let jwk: JWK<Empty> = JWK::deserialize(&mut j)?;
     jwk_to_rsa_key_params(jwk)
 }
 
-pub fn jwk_to_rsa_key_params(jwk: JWK<Empty>) -> Result<RSAKeyParameters, String> {
+pub fn jwk_to_rsa_key_params(jwk: JWK<Empty>) -> Result<RSAKeyParameters, Error> {
     if let AlgorithmParameters::RSA(x) = jwk.algorithm {
         Ok(x)
     } else {
-        Err(String::from("no rsa jwk"))
+        Err(KeyError::NoRsaJwk.into())
     }
 }
